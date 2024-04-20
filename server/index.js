5d78823e6c56ec6dd36ab327194f635774da55a1
Collin Chiang <collin.chiang1@gmail.com>
2024-04-20 12:13:56 -0400

    implemented connection to mongodb via mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URI)
+    .catch((error) => {
+        console.log(`MongoDB failed to connect...`);
+    });
+
+mongoose.connection.on("error", (error) => {
+    console.log(error);
+});
+
+const apiRoutes = require("./routes/api");
 
 
 const app = express();
+
+// configure middleware
+app.use(cors());
+app.use(express.json());
+
+// configure routes
+app.use("/api", apiRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Starting server on port ${process.env.PORT}...`)
